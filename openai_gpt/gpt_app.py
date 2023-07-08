fca4a67880f440cf2c71c1a86c9f4deb638faec0
qquinyri1 <128225478+qquinyri1@users.noreply.github.com>
2023-07-09 00:08:35 +0300

    fixed bugs

--- openai_gpt/gpt_app.py
+++ openai_gpt/gpt_app.py
@@ -12,75 +12,59 @@ COMMANDS = {
 END_COMMANS = ['exit', 'good bye', 'close']
 
 def gpt_app():
-    while True:
-        try:
+    try:
+        file_path = os.path.join(os.path.expanduser("~"), "key.json")
         
-            print(
-                '>>> Hello, I\'m your personal bot-assistant.'
-            )
-            if os.path.exists('key'):
-                with open('key', 'r') as fh: openai.api_key = fh.read().strip('"')
-            else:
-                key = input(
-                '>>> pls enter ur open AI key:'
-            )
-                if key:
-                    choose = input(
-                '>>> do u wanna save ur API key?, enter yes|not'
-            )
-                    if choose == 'yes':
-                        with open('key','w') as fh:
-                            json.dump(key,fh)
-                        openai.api_key = key
-                    else:
-                        openai.api_key = key
+        if os.path.exists(file_path):
+            with open(file_path, 'r') as fh:
+                openai.api_key = fh.read().strip('"')
+        else:
+            key = input('>>> будь ласка, введіть ключ AI:')
+            if key:
+                choose = input('>>> ви хочете зберегти свій ключ API?, введіть так|ні:')
+                if choose == 'yes':
+                    with open(file_path, 'w') as fh:
+                        json.dump(key, fh)
+                    openai.api_key = key
                 else:
-                    gpt_app()
-                 
-            is_working = True 
-            while is_working:
-            
-                story = 'You are a enceclopedia named "Valera" and trained to explain different topics and terms in short.'
-                user_input = input(
-                '>>> (valera) '
-            )
-                command, arguments = commands_parser(user_input)
-                if command in COMMANDS:
-                    command_handler = COMMANDS[command]
-                    story = command_handler(arguments)
-                    print(story)
-                elif command in END_COMMANS:
-                    print(
-                '>>> Сподіваюсь ти скоро повернешся та не забудь поповнити свій баланс.'
-            )
-                    is_working = False
-                else:
-                    gpt_answer(arguments,
-                                story
-            )
-        except KeyboardInterrupt:
-            print(
-                '\n>>> Будь ласка, користуйся командами для завершення роботи боту.'
-            )
-        except AuthenticationError:
-            print(
-                '>>> упс...відбулася помилка аунтефікації. Будь ласка, введіть коректний API ключ, який можна отримати на сайті: https://platform.openai.com/account/api-keys'
-            )
-            if os.path.exists('key'):
-                os.remove('key')
-                gpt_app()
+                    openai.api_key = key
             else:
                 gpt_app()
-        except ServiceUnavailableError:
-            print(
-                '>>> на даний момент сервер переповнений, будь ласка, повторіть спробу пізніше або відсутнє підключення до Інтернету'
-            )
+
+        is_working = True
+        while is_working:
+            story = 'Ви - енциклопедія під назвою "Валера" і навчені коротко пояснювати різні теми та терміни.'
+            user_input = input('>>> (valera) ')
+
+            command, arguments = commands_parser(user_input)
+            if command in COMMANDS:
+                command_handler = COMMANDS[command]
+                story = command_handler(arguments)
+                print(story)
+            elif command in END_COMMANS:
+                print('>>> Сподіваюсь, ти скоро повернешся та не забудеш поповнити свій баланс.')
+                is_working = False
+            else:
+                gpt_answer(arguments, story)
+
+    except KeyboardInterrupt:
+        print('\n>>> Будь ласка, користуйся командами для завершення роботи боту.')
+    except AuthenticationError:
+        print('>>> Упс... відбулася помилка аутентифікації. Будь ласка, введи коректний API ключ, який можна отримати на сайті: https://platform.openai.com/account/api-keys')
+        if os.path.exists(file_path):
+            os.remove(file_path)
             gpt_app()
-        except Exception as e:
-            print(e)
+        else:
+            gpt_app()
+    except ServiceUnavailableError:
+        print('>>> На даний момент сервер переповнений. Будь ласка, повтори спробу пізніше або перевір з\'єднання з Інтернетом.')
+        gpt_app()
+    except Exception as e:
+        print(e)
 
 if __name__ == "__main__":
+    print('>>> Привіт, я ваш персональний бот-помічник.\n')
     gpt_app()
-    
+
        
                     
